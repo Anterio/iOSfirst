3a59bd1a21ec653ea9e4f05f9752168a7dcabbf0
Anterio <Arthur@Arthurs-MacBook-Pro-2.local>
2014-07-24 09:12:15 -0400

    Thursday_ListAppChanges

--- List App/MFLTableViewController.m
+++ List App/MFLTableViewController.m
@@ -8,13 +8,21 @@
 
 #import "MFLTableViewController.h"
 
-
+/*
+ Pointers
+ TableViewController (maybe do one tutorial on this somewhere)
+*/
 
 @interface MFLTableViewController ()
-
-@property (nonatomic) NSArray * listItems;
+@property (nonatomic) NSArray * info;
+//@property (nonatomic) NSArray * days;
+//@property (nonatomic) NSArray * studentNames;
+//@property (nonatomic) NSArray * colors;
+//@property (nonatomic) NSArray * fontSize;
 
 @end
+
+
 
 @implementation MFLTableViewController
 
@@ -24,24 +32,65 @@
     if (self) {
         
         // Custom initialization
+        //        self.listItems = [[NSArray alloc] initWithObjects:@"Monday", @"Tuesday", @"Wednesday", @"Thursday", nil];
+        //
+        //        self.listItems = [NSArray arrayWithObjects:@"Monday", @"Tuesday", @"Wednesday", @"Thursday", nil];
         
-//        self.listItems = [[NSArray alloc] initWithObjects:@"Monday", @"Tuesday", @"Wednesday", @"Thursday", nil];
-//        
-//        self.listItems = [NSArray arrayWithObjects:@"Monday", @"Tuesday", @"Wednesday", @"Thursday", nil];
-       
-        self.listItems = @[@"Monday", @"Tuesday", @"Wednesday", @"Thursday"];
-        
-        
-
-}
+//        self.days = @[@"Monday", @"Tuesday", @"Wednesday", @"Thursday", @"Friday", @"Saturday", @"Sunday"];
+//        self.studentNames = @[@"John", @"Mary", @"Jayke", @"Steve", @"Alex", @"Patty", @"Suzy"];
+//        self.colors = @[[UIColor yellowColor],[UIColor redColor], [UIColor blackColor], [UIColor blueColor], [UIColor greenColor], [UIColor purpleColor], [UIColor brownColor]];
+//        self.fontSize = @[@20, @25, @30, @35, @40, @22, @33];
+        self.info = @[
+                      @{@"day":@"Monday",
+                        @"color": [UIColor yellowColor],
+                        @"name":@"John",
+                        @"size":@20
+                        },
+                      @{@"day":@"Tuesday",
+                        @"color": [UIColor redColor],
+                        @"name":@"Mary",
+                        @"size":@23
+                        },
+                      @{@"day":@"Wednesday",
+                        @"color": [UIColor blackColor],
+                        @"name":@"Jayke",
+                        @"size":@26
+                        },
+                      @{@"day":@"Thursday",
+                        @"color": [UIColor blueColor],
+                        @"name":@"Steve",
+                        @"size":@29
+                        },
+                      @{@"day":@"Friday",
+                        @"color": [UIColor greenColor],
+                        @"name":@"Alex",
+                        @"size":@32
+                        },
+                      @{@"day":@"Saturday",
+                        @"color": [UIColor purpleColor],
+                        @"name":@"Patty",
+                        @"size":@35
+                        },
+                      @{@"day":@"Sunday",
+                        @"color": [UIColor brownColor],
+                        @"name":@"Suzy",
+                        @"size":@38
+                        }
+                      ];
+    }
     return self;
 }
 
--(NSArray*)listItems
-{
-    if (_listItems == nil) _listItems = @[];
-    return _listItems;
-}
+//-(NSArray*)days
+//{
+//    if (_days == nil) _days = @[];
+//    return _days;
+//}
+//-(NSArray*)studentNames
+//{
+//    if (_studentNames == nil) _studentNames = @[];
+//    return _studentNames;
+//}
 
 - (void)viewDidLoad
 {
@@ -54,36 +103,58 @@
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
 #pragma mark - Table view data source
-
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
     // Return the number of rows in the section.
-    return self.listItems.count;
+    return self.info.count;
 }
 
 
-
+/*
+ 
+ <Type/Class> <variable name> = <value>
+ 
+ int myAge = 19;
+ UIView* myRedBanner = [UIView ...]
+ 
+ 
+ */
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    UITableViewCell* cell = [[UITableViewCell alloc] init];
+    //Just don't worry about reuseIdentifier for now, Jo will explain in a few weeks
+    UITableViewCell* cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:nil];
     
-//    NSString * listItem = [self.listItems objectAtIndex:indexPath.row];
+    int indexOfItem = indexPath.row; //helper local variable that we can reuse
     
-    NSString * listItem = self.listItems[indexPath.row];
+    NSLog(@"Want to draw the %d element", indexOfItem);
+    NSDictionary* infoItem = self.info[indexPath.row];
+    
 
-        NSLog(@"listItem = %@", listItem);
-    cell.textLabel.text = listItem;
-    cell.backgroundColor = [UIColor greenColor];
+    //NSString * day = [self.listItems objectAtIndex:indexOfItem];
+//    NSString* day = self.days[indexOfItem];
     
+    NSString * studentName = infoItem [@"name"];
+    NSNumber* font = infoItem [@"size"];
+    
+    cell.textLabel.font = [UIFont systemFontOfSize:[font intValue]];
+    
+    //TODO make a variable 'currentColor' to get the color from your color array (just like you got the student name etc)
+    
+    //
     // Configure the cell...
+    //
+    
+    cell.textLabel.text = infoItem[@"day"];
+    cell.detailTextLabel.text = studentName;
+    cell.backgroundColor = infoItem[@"color"];
+    
+    //set background color to color in array
+   //TODO instead of hardcoding to green, rather use variale currentColor
+    
+    //set the textlabel font size to a number from last array
+    
     
     return cell;
 }
@@ -99,43 +170,33 @@
 
 
 /*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
+ // Override to support editing the table view.
+ - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+ {
+ if (editingStyle == UITableViewCellEditingStyleDelete) {
+ // Delete the row from the data source
+ [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+ } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+ // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+ }
+ }
+ */
 
 /*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
+ // Override to support rearranging the table view.
+ - (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
+ {
+ }
+ */
 
 /*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
+ // Override to support conditional rearranging of the table view.
+ - (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+ {
+ // Return NO if you do not want the item to be re-orderable.
+ return YES;
+ }
+ */
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
